6184065dca4f360b9ef4e3669db358d55dd77626
Will <xlythe@gmail.com>
2013-09-15 14:02:11 -0400

    update version number

--- src/com/xlythe/saolauncher/smsextension/receiver/SmsReceiver.java
+++ src/com/xlythe/saolauncher/smsextension/receiver/SmsReceiver.java
@@ -1,20 +1,19 @@
 package com.xlythe.saolauncher.smsextension.receiver;
 
-import com.xlythe.saolauncher.smsextension.BuildConfig;
-import com.xlythe.saolauncher.smsextension.service.SmsReceiverService;
-import com.xlythe.saolauncher.smsextension.util.Log;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import com.commonsware.cwac.wakeful.WakefulIntentService;
+import com.xlythe.saolauncher.smsextension.BuildConfig;
+import com.xlythe.saolauncher.smsextension.service.SmsReceiverService;
+import com.xlythe.saolauncher.smsextension.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (BuildConfig.DEBUG)
-            Log.v("SMSReceiver: onReceive()");
+        if(BuildConfig.DEBUG) Log.v("SMSReceiver: onReceive()");
         intent.setClass(context, SmsReceiverService.class);
         intent.putExtra("result", getResultCode());
         WakefulIntentService.sendWakefulWork(context.getApplicationContext(), intent);
