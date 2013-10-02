1c34fedab13dc9070ff263868f63981bf449145f
Xlythe <xlythe@gmail.com>
2013-10-01 20:12:28 -0400

    update version number

--- src/com/xlythe/saolauncher/smsextension/ui/SmsPopupFragment.java
+++ src/com/xlythe/saolauncher/smsextension/ui/SmsPopupFragment.java
@@ -2,12 +2,6 @@ package com.xlythe.saolauncher.smsextension.ui;
 
 import java.lang.ref.WeakReference;
 
-import com.xlythe.saolauncher.smsextension.BuildConfig;
-import com.xlythe.saolauncher.smsextension.R;
-import com.xlythe.saolauncher.smsextension.preferences.ButtonListPreference;
-import com.xlythe.saolauncher.smsextension.provider.SmsMmsMessage;
-import com.xlythe.saolauncher.smsextension.util.Log;
-import com.xlythe.saolauncher.smsextension.util.SmsPopupUtils;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -30,6 +24,13 @@ import android.widget.QuickContactBadge;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
+
+import com.xlythe.saolauncher.smsextension.BuildConfig;
+import com.xlythe.saolauncher.smsextension.R;
+import com.xlythe.saolauncher.smsextension.preferences.ButtonListPreference;
+import com.xlythe.saolauncher.smsextension.provider.SmsMmsMessage;
+import com.xlythe.saolauncher.smsextension.util.Log;
+import com.xlythe.saolauncher.smsextension.util.SmsPopupUtils;
 
 public class SmsPopupFragment extends Fragment {
     private SmsMmsMessage message;
@@ -78,37 +79,35 @@ public class SmsPopupFragment extends Fragment {
     private static final int BUTTON_SWITCHER_UNLOCK_BUTTON = 1;
     private static final String EMPTY_MMS_SUBJECT = "no subject";
 
-    public static SmsPopupFragment newInstance(SmsMmsMessage newMessage, int[] buttons,
-    		int privacyMode, boolean showUnlockButton, boolean showButtons) {
+    public static SmsPopupFragment newInstance(SmsMmsMessage newMessage, int[] buttons, int privacyMode, boolean showUnlockButton, boolean showButtons) {
 
-    	SmsPopupFragment newFragment = new SmsPopupFragment();
-    	Bundle args = newMessage.toBundle();
-    	args.putInt(EXTRA_PRIVACY_MODE, privacyMode);
-    	args.putIntArray(EXTRA_BUTTONS, buttons);
-    	args.putBoolean(EXTRA_UNLOCK_BUTTON, showUnlockButton);
-    	args.putBoolean(EXTRA_SHOW_BUTTONS, showButtons);
-    	newFragment.setArguments(args);
-    	return newFragment;
+        SmsPopupFragment newFragment = new SmsPopupFragment();
+        Bundle args = newMessage.toBundle();
+        args.putInt(EXTRA_PRIVACY_MODE, privacyMode);
+        args.putIntArray(EXTRA_BUTTONS, buttons);
+        args.putBoolean(EXTRA_UNLOCK_BUTTON, showUnlockButton);
+        args.putBoolean(EXTRA_SHOW_BUTTONS, showButtons);
+        newFragment.setArguments(args);
+        return newFragment;
     }
 
     public static SmsPopupFragment newInstance(SmsMmsMessage newMessage, int[] buttons) {
-    	return newInstance(newMessage, buttons, PRIVACY_MODE_OFF, false, true);
+        return newInstance(newMessage, buttons, PRIVACY_MODE_OFF, false, true);
     }
 
     public SmsPopupFragment() {};
 
-	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-    		Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-	    final Bundle args = getArguments();
+        final Bundle args = getArguments();
         privacyMode = args.getInt(EXTRA_PRIVACY_MODE);
         showUnlockButton = args.getBoolean(EXTRA_UNLOCK_BUTTON);
         showButtons = args.getBoolean(EXTRA_SHOW_BUTTONS);
         final int[] buttons = args.getIntArray(EXTRA_BUTTONS);
         message = new SmsMmsMessage(getActivity(), args);
 
-    	View v = inflater.inflate(R.layout.popup_message_fragment, container, false);
+        View v = inflater.inflate(R.layout.popup_message_fragment, container, false);
 
         // Find the main textviews and layouts
         fromTv = (TextView) v.findViewById(R.id.fromTextView);
@@ -126,7 +125,7 @@ public class SmsPopupFragment extends Fragment {
 
         final String[] buttonText = getResources().getStringArray(R.array.buttons_text);
 
-        if (showButtons) {
+        if(showButtons) {
             final Button button1 = (Button) v.findViewById(R.id.button1);
             final PopupButton button1Vals = new PopupButton(buttons[0], buttonText);
             button1.setOnClickListener(button1Vals);
@@ -146,25 +145,20 @@ public class SmsPopupFragment extends Fragment {
             button3.setText(button3Vals.buttonText);
 
             /*
-             * This is really hacky. There are two types of reply buttons (quick reply and reply).
-             * If the user has selected to show both the replies then the text on the buttons
-             * should be different. If they only use one then the text can just be "Reply".
+             * This is really hacky. There are two types of reply buttons (quick
+             * reply and reply). If the user has selected to show both the
+             * replies then the text on the buttons should be different. If they
+             * only use one then the text can just be "Reply".
              */
             int numReplyButtons = 0;
-            if (button1Vals.isReplyButton)
-                numReplyButtons++;
-            if (button2Vals.isReplyButton)
-                numReplyButtons++;
-            if (button3Vals.isReplyButton)
-                numReplyButtons++;
+            if(button1Vals.isReplyButton) numReplyButtons++;
+            if(button2Vals.isReplyButton) numReplyButtons++;
+            if(button3Vals.isReplyButton) numReplyButtons++;
 
-            if (numReplyButtons == 1) {
-                if (button1Vals.isReplyButton)
-                    button1.setText(R.string.button_reply);
-                if (button2Vals.isReplyButton)
-                    button2.setText(R.string.button_reply);
-                if (button3Vals.isReplyButton)
-                    button3.setText(R.string.button_reply);
+            if(numReplyButtons == 1) {
+                if(button1Vals.isReplyButton) button1.setText(R.string.button_reply);
+                if(button2Vals.isReplyButton) button2.setText(R.string.button_reply);
+                if(button3Vals.isReplyButton) button3.setText(R.string.button_reply);
             }
 
             ((Button) v.findViewById(R.id.unlockButton)).setOnClickListener(new OnClickListener() {
@@ -175,27 +169,26 @@ public class SmsPopupFragment extends Fragment {
             });
         }
 
-        if (message.isMms()) {
+        if(message.isMms()) {
             // The ViewMMS button
-            ((Button) v.findViewById(R.id.viewMmsButton)).setOnClickListener(
-                    new PopupButton(BUTTON_VIEW_MMS, buttonText));
+            ((Button) v.findViewById(R.id.viewMmsButton)).setOnClickListener(new PopupButton(BUTTON_VIEW_MMS, buttonText));
             final String mmsSubject = message.getMessageBody();
             final TextView mmsSubjectTV = ((TextView) v.findViewById(R.id.mmsSubjectTextView));
-            if (mmsSubject != null && !"".equals(mmsSubject) && !EMPTY_MMS_SUBJECT.equals(mmsSubject)) {
+            if(mmsSubject != null && !"".equals(mmsSubject) && !EMPTY_MMS_SUBJECT.equals(mmsSubject)) {
                 mmsSubjectTV.setText(message.getMessageBody());
-            } else {
+            }
+            else {
                 mmsSubjectTV.setVisibility(View.GONE);
             }
         }
 
         // The privacy view message button
-        ((ImageButton) v.findViewById(R.id.viewButton)).setOnClickListener(
-                new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mButtonsListener.onButtonClicked(BUTTON_VIEW);
-                    }
-                });
+        ((ImageButton) v.findViewById(R.id.viewButton)).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButtonsListener.onButtonClicked(BUTTON_VIEW);
+            }
+        });
 
         populateViews();
 
@@ -206,18 +199,19 @@ public class SmsPopupFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-        	mButtonsListener = (SmsPopupButtonsListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-            		+ " must implement SmsPopupButtonsListener");
+            mButtonsListener = (SmsPopupButtonsListener) activity;
+        }
+        catch(ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement SmsPopupButtonsListener");
         }
     }
 
     /*
-     * Populate all the main SMS/MMS views with content from the actual SmsMmsMessage
+     * Populate all the main SMS/MMS views with content from the actual
+     * SmsMmsMessage
      */
     private void populateViews() {
-        if (message.isSms()) {
+        if(message.isSms()) {
             messageTv.setText(message.getMessageBody());
         }
 
@@ -236,24 +230,26 @@ public class SmsPopupFragment extends Fragment {
     }
 
     public void setShowUnlockButton(boolean show) {
-        if (show != showUnlockButton) {
+        if(show != showUnlockButton) {
             showUnlockButton = show;
             refreshButtonViews();
         }
     }
 
     private void refreshButtonViews() {
-        if (!showButtons) {
+        if(!showButtons) {
             buttonViewSwitcher.setVisibility(View.GONE);
-        } else {
+        }
+        else {
             final int currentView = buttonViewSwitcher.getDisplayedChild();
-            if (showUnlockButton) {
-                if (currentView != BUTTON_SWITCHER_UNLOCK_BUTTON) {
+            if(showUnlockButton) {
+                if(currentView != BUTTON_SWITCHER_UNLOCK_BUTTON) {
                     // Show unlock button
                     buttonViewSwitcher.setDisplayedChild(BUTTON_SWITCHER_UNLOCK_BUTTON);
                 }
-            } else {
-                if (currentView != BUTTON_SWITCHER_MAIN_BUTTONS) {
+            }
+            else {
+                if(currentView != BUTTON_SWITCHER_MAIN_BUTTONS) {
                     // Show main popup buttons
                     buttonViewSwitcher.setDisplayedChild(BUTTON_SWITCHER_MAIN_BUTTONS);
                 }
@@ -262,42 +258,42 @@ public class SmsPopupFragment extends Fragment {
     }
 
     public void setPrivacy(int newMode) {
-    	setPrivacy(newMode, false);
+        setPrivacy(newMode, false);
     }
 
     private void setPrivacy(int newMode, boolean initial) {
 
-        if ((newMode != privacyMode || initial) && message != null) {
-	        final int viewPrivacy =
-	                message.isSms() ? VIEW_PRIVACY_SMS : VIEW_PRIVACY_MMS;
+        if((newMode != privacyMode || initial) && message != null) {
+            final int viewPrivacy = message.isSms() ? VIEW_PRIVACY_SMS : VIEW_PRIVACY_MMS;
 
-	        final int viewPrivacyOff =
-	                message.isSms() ? VIEW_SMS : VIEW_MMS;
+            final int viewPrivacyOff = message.isSms() ? VIEW_SMS : VIEW_MMS;
 
-	        if (newMode == PRIVACY_MODE_OFF) {
+            if(newMode == PRIVACY_MODE_OFF) {
                 updateContentView(viewPrivacyOff);
-	            fromTv.setVisibility(View.VISIBLE);
-	            messageViewed = true;
-	            if (initial || privacyMode == PRIVACY_MODE_HIDE_ALL) {
-	            	loadContactPhoto();
-	            }
-	        } else if (newMode == PRIVACY_MODE_HIDE_MESSAGE) {
+                fromTv.setVisibility(View.VISIBLE);
+                messageViewed = true;
+                if(initial || privacyMode == PRIVACY_MODE_HIDE_ALL) {
+                    loadContactPhoto();
+                }
+            }
+            else if(newMode == PRIVACY_MODE_HIDE_MESSAGE) {
                 updateContentView(viewPrivacy);
-	            fromTv.setVisibility(View.VISIBLE);
-	            loadContactPhoto();
-	        } else if (newMode == PRIVACY_MODE_HIDE_ALL) {
-	            updateContentView(viewPrivacy);
-	            fromTv.setVisibility(View.GONE);
-	        }
-    	}
-    	privacyMode = newMode;
+                fromTv.setVisibility(View.VISIBLE);
+                loadContactPhoto();
+            }
+            else if(newMode == PRIVACY_MODE_HIDE_ALL) {
+                updateContentView(viewPrivacy);
+                fromTv.setVisibility(View.GONE);
+            }
+        }
+        privacyMode = newMode;
     }
 
     private void updateContentView(int mode) {
-        if (contentMessage != null && contentMms != null && contentPrivacy != null) {
-            if (contentNum != mode) {
+        if(contentMessage != null && contentMms != null && contentPrivacy != null) {
+            if(contentNum != mode) {
                 contentNum = mode;
-                switch (mode) {
+                switch(mode) {
                 case VIEW_SMS:
                     contentMessage.setVisibility(View.VISIBLE);
                     contentMms.setVisibility(View.GONE);
@@ -319,28 +315,29 @@ public class SmsPopupFragment extends Fragment {
     }
 
     private void loadContactPhoto() {
-    	boolean cacheHit = false;
-    	if (mButtonsListener != null && message.getContactLookupUri() != null) {
-    		final LruCache<Uri, Bitmap> cache = mButtonsListener.getCache();
-    		if (cache != null) {
-    			final Bitmap bitmap = cache.get(message.getContactLookupUri());
-    			if (bitmap != null) {
-    				if (BuildConfig.DEBUG) Log.v("loadContactPhoto() - bitmap cache hit");
-    				contactBadge.setImageBitmap(bitmap);
-    				cacheHit = true;
-    			}
-    		}
-    	}
+        boolean cacheHit = false;
+        if(mButtonsListener != null && message.getContactLookupUri() != null) {
+            final LruCache<Uri, Bitmap> cache = mButtonsListener.getCache();
+            if(cache != null) {
+                final Bitmap bitmap = cache.get(message.getContactLookupUri());
+                if(bitmap != null) {
+                    if(BuildConfig.DEBUG) Log.v("loadContactPhoto() - bitmap cache hit");
+                    contactBadge.setImageBitmap(bitmap);
+                    cacheHit = true;
+                }
+            }
+        }
 
-    	if (!cacheHit) {
-    		new FetchContactPhotoTask(contactBadge).execute(message.getContactLookupUri());
-    	}
+        if(!cacheHit) {
+            new FetchContactPhotoTask(contactBadge).execute(message.getContactLookupUri());
+        }
 
         contactBadge.setClickable(true);
         final Uri contactUri = message.getContactLookupUri();
-        if (contactUri != null) {
+        if(contactUri != null) {
             contactBadge.assignContactUri(message.getContactLookupUri());
-        } else {
+        }
+        else {
             contactBadge.assignContactFromPhone(message.getAddress(), false);
         }
     }
@@ -349,24 +346,23 @@ public class SmsPopupFragment extends Fragment {
      * AsyncTask to fetch contact photo in background
      */
     private class FetchContactPhotoTask extends AsyncTask<Uri, Integer, Bitmap> {
-    	private final WeakReference<QuickContactBadge> viewReference;
+        private final WeakReference<QuickContactBadge> viewReference;
 
-    	public FetchContactPhotoTask(QuickContactBadge badge) {
-    		viewReference = new WeakReference<QuickContactBadge> (badge);
-    	}
+        public FetchContactPhotoTask(QuickContactBadge badge) {
+            viewReference = new WeakReference<QuickContactBadge>(badge);
+        }
 
         @Override
         protected Bitmap doInBackground(Uri... params) {
-            if (isAdded()) {
-                if (BuildConfig.DEBUG)
-                    Log.v("Loading contact photo in background...");
+            if(isAdded()) {
+                if(BuildConfig.DEBUG) Log.v("Loading contact photo in background...");
                 final Bitmap bitmap = SmsPopupUtils.getPersonPhoto(getActivity(), params[0]);
-            	if (mButtonsListener != null && bitmap != null) {
-            		final LruCache<Uri, Bitmap> cache = mButtonsListener.getCache();
-            		if (cache != null) {
-            			cache.put(params[0], bitmap);
-            		}
-            	}
+                if(mButtonsListener != null && bitmap != null) {
+                    final LruCache<Uri, Bitmap> cache = mButtonsListener.getCache();
+                    if(cache != null) {
+                        cache.put(params[0], bitmap);
+                    }
+                }
 
                 return bitmap;
             }
@@ -375,19 +371,16 @@ public class SmsPopupFragment extends Fragment {
 
         @Override
         protected void onPostExecute(Bitmap photo) {
-            if (BuildConfig.DEBUG)
-                Log.v("Done loading contact photo");
-            if (photo != null && viewReference != null) {
-            	final QuickContactBadge badge = viewReference.get();
-            	if (badge != null && isAdded()) {
-	                TransitionDrawable mTd =
-	                        new TransitionDrawable(new Drawable[] {
-	                                getResources().getDrawable(R.drawable.ic_contact_picture),
-	                                new BitmapDrawable(getResources(), photo) });
-	                badge.setImageDrawable(mTd);
-	                mTd.setCrossFadeEnabled(false);
-	                mTd.startTransition(CONTACT_IMAGE_FADE_DURATION);
-            	}
+            if(BuildConfig.DEBUG) Log.v("Done loading contact photo");
+            if(photo != null && viewReference != null) {
+                final QuickContactBadge badge = viewReference.get();
+                if(badge != null && isAdded()) {
+                    TransitionDrawable mTd = new TransitionDrawable(new Drawable[] { getResources().getDrawable(R.drawable.ic_contact_picture),
+                            new BitmapDrawable(getResources(), photo) });
+                    badge.setImageDrawable(mTd);
+                    mTd.setCrossFadeEnabled(false);
+                    mTd.startTransition(CONTACT_IMAGE_FADE_DURATION);
+                }
             }
         }
     }
@@ -401,30 +394,30 @@ public class SmsPopupFragment extends Fragment {
         public PopupButton(int id, String[] buttonTextArray) {
             buttonId = id;
             isReplyButton = false;
-            if (buttonId == ButtonListPreference.BUTTON_REPLY
-                    || buttonId == ButtonListPreference.BUTTON_QUICKREPLY
+            if(buttonId == ButtonListPreference.BUTTON_REPLY || buttonId == ButtonListPreference.BUTTON_QUICKREPLY
                     || buttonId == ButtonListPreference.BUTTON_REPLY_BY_ADDRESS) {
                 isReplyButton = true;
             }
 
-            if (buttonId < buttonTextArray.length) {
-            	buttonText = buttonTextArray[buttonId];
+            if(buttonId < buttonTextArray.length) {
+                buttonText = buttonTextArray[buttonId];
             }
 
-            if (buttonId == ButtonListPreference.BUTTON_DISABLED) { // Disabled
+            if(buttonId == ButtonListPreference.BUTTON_DISABLED) { // Disabled
                 buttonVisibility = View.GONE;
             }
         }
 
         @Override
         public void onClick(View v) {
-        	mButtonsListener.onButtonClicked(buttonId);
+            mButtonsListener.onButtonClicked(buttonId);
         }
     }
 
     public static interface SmsPopupButtonsListener {
-    	abstract void onButtonClicked(int buttonType);
-    	abstract LruCache<Uri, Bitmap> getCache();
+        abstract void onButtonClicked(int buttonType);
+
+        abstract LruCache<Uri, Bitmap> getCache();
     }
 
 }
